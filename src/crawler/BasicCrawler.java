d6ae466a3f8c5d4e0609c31a5f068991d8754510
Mihai C <mciorobe@adobe.com>
2013-12-02 00:31:32 +0200

    Refactor: class moved + copyright deleted

--- src/crawler/BasicCrawler.java
+++ src/crawler/BasicCrawler.java
@@ -1,59 +1,61 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package edu.uci.ics.crawler4j.examples.basic;
+package crawler;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
-import org.apache.http.Header;
-
-/**
- * @author Yasser Ganjisaffar <lastname at gmail dot com>
- */
 public class BasicCrawler extends WebCrawler {
 
     private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4"
             + "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
-    /**
-     * You should implement this function to specify whether the given url
-     * should be crawled or not (based on your crawling logic).
-     */
     @Override
     public boolean shouldVisit(WebURL url) {
         String href = url.getURL().toLowerCase();
         return !FILTERS.matcher(href).matches() && href.startsWith("http://automaticacalculatoare.wordpress.com/");
     }
 
-    /**
-     * This function is called when a page is fetched and ready to be processed
-     * by your program.
-     */
     @Override
     public void visit(Page page) {
 
-        if (((HtmlParseData)page.getParseData()).getHtml().contains("textarea"))
+        String htmlContent = ((HtmlParseData) page.getParseData()).getHtml();
+        if (htmlContent.contains("textarea")) {
             System.err.println(page.getWebURL().toString());
+
+            int index = 0;
+            boolean inTag = false;
+            StringBuilder strepBody = new StringBuilder(htmlContent);
+            System.out.println(strepBody.toString());
+
+            while (index < strepBody.length()) {
+                if (inTag) {
+                    if (strepBody.charAt(index) == '>') {
+                        inTag = false;
+                    }
+                    index++;
+                } else {
+                    if (strepBody.charAt(index) == '<') {
+                        inTag = true;
+                        index++;
+                    } else {
+                        strepBody.deleteCharAt(index);
+                    }
+                }
+            }
+
+
+            System.out.println(strepBody.toString());
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
 //        int docid = page.getWebURL().getDocid();
 //        String url = page.getWebURL().getURL();
 //        String domain = page.getWebURL().getDomain();
